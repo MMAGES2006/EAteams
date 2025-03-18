9d854d94d1e7422f170e3f8bf03f15127dc08fa8
B) <22089@portalcedi.edu.mx>
2025-03-18 09:27:14 -0600

    registro

--- src/app/resgistro/resgistro.page.ts
+++ src/app/resgistro/resgistro.page.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonItem, IonLabel } from '@ionic/angular/standalone';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+
 
 @Component({
   selector: 'app-resgistro',
@@ -12,9 +16,41 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
 })
 export class ResgistroPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertController: AlertController, private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
   }
 
+  email: string = '';
+  password: string = '';
+
+  async onSubmit() {
+    try {
+      await this.authService.register(this.email, this.password);
+      const alert = await this.alertController.create({
+        header: 'Signup Success',
+        message: 'You have created successfully',
+        buttons: ['OK'],
+      });
+      await alert.present();
+      this.router.navigate(['/login']);
+    } catch (error) {
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Please complete all',
+        buttons: ['OK'],
+      });
+      await alert.present();
+    }
+  }
+  validateEmail(email: string): boolean {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailPattern.test(email);
+  }
+
+  onSignUp() {
+    this.router.navigateByUrl("registro")
+  }
 }
+
+
